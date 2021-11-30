0cd2ceff167e2cada6e04bd516f89623e97efe1d
exponentialR <sadebayo01@qub.ac.uk>
2021-11-30 19:48:15 +0100

    Add_two_numbers.py

--- Add_two_numbers.py
+++ Add_two_numbers.py
@@ -17,36 +17,49 @@ Input: l1 = [9,9,9,9,9,9,9], l2 = [9,9,9,9]
 Output: [8,9,9,9,0,0,0,1]
 """
 class ListNode:
-    def __init__(self, val = 0, next = None):
-        self.val = val
-        self.next = next
+    def __init__(self, x):
+        self.val = x
+        self.next = None
 class Solution:
     def addTwoNumbers(self, input1:ListNode, input2: ListNode) -> ListNode:
+        prev = result = ListNode(None)
+        carry = 0
+        while input1 or input2 or carry:
+            if input1:
+                carry += input1.val
+                input1 = input1.next
+            if input2:
+                carry += input2.val
+                input2 = input2.next
+            prev.next = ListNode(carry % 10 )
+            prev = prev.next
+            carry //=10
+        return result.next
 
-        added = ListNode(val = (input1.val, input2.val)%10)
-        carryover = (input1.val + input2.val) //10
-        currntnode = added
-        while (input1.next and input2.next):
-            input1 = input1.next
-            input2 = input2.next
-            currntnode.next = ListNode(val = (carryover + input1.val + input2.val) % 10)
-            carryover = (carryover + input1.val + input2.val)//10
-            currntnode = currntnode.next
-
-        while(input1.next):
-            input1 = input1.next
-            currntnode.next = ListNode(val=(carryover + input1.val) % 10)
-            carryover = (carryover + input1.val ) // 10
-            currntnode = currntnode.next
-
-        while(input2.next):
-            input2 = input2.next
-            currntnode.next = ListNode(val = (carryover + input2.val) % 10)
-            carryover = (carryover + input2.val) // 10
-            currntnode = currntnode.next
-        if (carryover) > 0:
-            currntnode.next = ListNode(val = 1)
-        return added
+        # added = ListNode(val = (input1.val, input2.val)%10)
+        # carryover = (input1.val + input2.val) //10
+        # currntnode = added
+        # while (input1.next and input2.next):
+        #     input1 = input1.next
+        #     input2 = input2.next
+        #     currntnode.next = ListNode(val = (carryover + input1.val + input2.val) % 10)
+        #     carryover = (carryover + input1.val + input2.val)//10
+        #     currntnode = currntnode.next
+        #
+        # while(input1.next):
+        #     input1 = input1.next
+        #     currntnode.next = ListNode(val=(carryover + input1.val) % 10)
+        #     carryover = (carryover + input1.val ) // 10
+        #     currntnode = currntnode.next
+        #
+        # while(input2.next):
+        #     input2 = input2.next
+        #     currntnode.next = ListNode(val = (carryover + input2.val) % 10)
+        #     carryover = (carryover + input2.val) // 10
+        #     currntnode = currntnode.next
+        # if (carryover) > 0:
+        #     currntnode.next = ListNode(val = 1)
+        # return added
 
 
     # int1 = ''
